35653d97ed7f42fc001fe3d28ab14fbb61cbb4ec
sachil <perryspcalpha@gmail.com>
2016-03-03 10:20:50 +0800

    添加启动欢迎界面。

--- app/src/main/java/com/sachil/essence/view/BaseActivity.java
+++ app/src/main/java/com/sachil/essence/view/BaseActivity.java
@@ -1,8 +1,11 @@
 package com.sachil.essence.view;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+    protected Handler UI_THREAD = new Handler(Looper.getMainLooper());
 
     @Override
     public void setContentView(int layoutResID) {
