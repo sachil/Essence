9a6d4992be58abf04ddb6fa6f8bdeb1b49b58e61
sachil <perryspcalpha@gmail.com>
2016-03-04 11:16:14 +0800

    初始化Navigation View，添加内容分类。

--- app/src/main/java/com/sachil/essence/view/BaseActivity.java
+++ app/src/main/java/com/sachil/essence/view/BaseActivity.java
@@ -3,6 +3,10 @@ package com.sachil.essence.view;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+
+import com.sachil.essence.R;
+import com.sachil.essence.StatusBarUtils;
 
 public class BaseActivity extends AppCompatActivity {
     protected Handler UI_THREAD = new Handler(Looper.getMainLooper());
@@ -13,7 +17,14 @@ public class BaseActivity extends AppCompatActivity {
         setStatusBar();
     }
 
-    protected void setStatusBar(){
-
+    protected int getPrimaryColor(){
+        TypedValue value = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, value, true);
+        return value.data;
     }
+
+    protected void setStatusBar() {
+        StatusBarUtils.setColor(this, getPrimaryColor());
+    }
+
 }
--- app/src/main/java/com/sachil/essence/view/MainActivity.java
+++ app/src/main/java/com/sachil/essence/view/MainActivity.java
@@ -1,17 +1,27 @@
 package com.sachil.essence.view;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.sachil.essence.R;
+import com.sachil.essence.StatusBarUtils;
 
 public class MainActivity extends BaseActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Toolbar mToolbar = null;
+    private NavigationView mNavigation = null;
+    private DrawerLayout mDrawer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
 
     @Override
@@ -23,5 +33,22 @@ public class MainActivity extends BaseActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void setStatusBar() {
+        StatusBarUtils.setColorForDrawerLayout(this, (DrawerLayout) findViewById(R.id.main_drawer), getPrimaryColor());
+    }
+
+    private void initView() {
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        mNavigation = (NavigationView) findViewById(R.id.main_navigation);
+        mDrawer = (DrawerLayout) findViewById(R.id.main_drawer);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
