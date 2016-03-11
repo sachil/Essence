3614e4bebccf11678d25f5e61c77360ea78fe378
sachil <perryspcalpha@gmail.com>
2016-03-11 16:30:33 +0800

    优化代码结构，采用mvp模式

---
+++ app/src/main/java/com/sachil/essence/ui/BaseActivity.java
@@ -0,0 +1,43 @@
+package com.sachil.essence.ui;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+
+import com.sachil.essence.R;
+import com.sachil.essence.StatusBarUtils;
+
+public class BaseActivity extends AppCompatActivity {
+    protected Handler UI_THREAD = new Handler(Looper.getMainLooper());
+    protected int mThemeColor = 0;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        setStatusBar();
+    }
+
+    protected int getThemeColor(){
+        if(mThemeColor == 0){
+            TypedValue value = new TypedValue();
+            getTheme().resolveAttribute(R.attr.colorPrimary, value, true);
+            return value.data;
+        }else
+            return mThemeColor;
+
+    }
+
+    protected int getNormalTextColor(){
+        return getResources().getColor(R.color.grey_600);
+    }
+
+    protected void setStatusBar() {
+        StatusBarUtils.setColor(this, getThemeColor());
+    }
+
+    protected void changeTheme(int color){
+        mThemeColor = color;
+    }
+
+}
